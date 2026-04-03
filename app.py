ed11c76229cdcd8c2fd56b2cfd2af31b784c066d
Chaitali0912 <chaitali09122005@gmail.com>
2026-04-03 13:47:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+# Ensure Streamlit Cloud can find local modules
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 import pandas as pd
 import torch
